470c5c860747093bea6730cace5bbc12a815fb09
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-13 23:01:01 +0330

    import 'package:clean_project/main_widget.dart'; import 'package:flutter/material.dart'; import 'package:flutter_riverpod/flutter_riverpod.dart';
    void main() {
      runApp(ProviderScope(child: MainWidget()));
    }

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:clean_project/main_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(ProviderScope(child: MainWidget()));
 }
